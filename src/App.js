35c89c77ac87ef7609f7bd7e9c6cb61136734fc6
kevin <kevin@oniondesign.com.tw>
2019-06-27 15:32:06 +0800

    add nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { Home, Books, Electronics } from './pages';
+import { Navbar } from './components';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         {/* The corresponding component will show here if the current URL matches the path */}
+        <Navbar />
         <Route path="/" exact component={Home} />
         <Route path="/books" component={Books} />
         <Route path="/electronics" component={Electronics} />
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import { Navbar } from './components';
 
 class Header extends Component {
   render() {
     return (
-      <Navbar />
+      <div className="container">
+      </div>
     );
   }
 }
